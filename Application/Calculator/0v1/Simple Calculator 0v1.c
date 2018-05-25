ce5d8855b3ade14a0a727f8d5ff894dc6a46dfa7
Arun <arun9047474939@gmail.com>
2018-05-25 21:55:56 +0530

    Add files via upload

---
+++ Application/Calculator/0v1/Simple Calculator 0v1.c
@@ -0,0 +1,43 @@
+//www.ArunEworld
+
+// Performs addition, subtraction, multiplication or division depending the input from user
+
+# include <stdio.h>
+char Get_Operator;
+
+int main() {
+
+    
+    double First_Number,Second_Number;
+
+    printf("Enter an operator (+, -, *,): ");
+    scanf("%c", &Get_Operator);
+
+    printf("Enter two operands: ");
+    scanf("%lf %lf",&First_Number, &Second_Number);
+
+    switch(Get_Operator)
+    {
+        case '+':
+            printf("%.1lf + %.1lf = %.1lf",First_Number, Second_Number, First_Number + Second_Number);
+            break;
+
+        case '-':
+            printf("%.1lf - %.1lf = %.1lf",First_Number, Second_Number, First_Number - Second_Number);
+            break;
+
+        case '*':
+            printf("%.1lf * %.1lf = %.1lf",First_Number, Second_Number, First_Number * Second_Number);
+            break;
+
+        case '/':
+            printf("%.1lf / %.1lf = %.1lf",First_Number, Second_Number, First_Number / Second_Number);
+            break;
+
+        // operator doesn't match any case constant (+, -, *, /)
+        default:
+            printf("Error! operator is not correct");
+    }
+    
+    return 0;
+}
